cf3d2a4eca7a06750ada0058f67405c808d78f7f
Dragonroar45 <malhotraharsh098@gmail.com>
2024-08-08 19:27:05 +0530

    Complete function getHumanChoice

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice(){
-    let numComputerChoice = Math.floor(Math.random() * (3-1+1)) + 1;
+    let numComputerChoice = Math.floor(Math.random() * (3-1+1)) + 1; // 1 implies rock, 2 implies scissor, 3 implies paper
     if (numComputerChoice === 1){
         return "rock";
     } else if (numComputerChoice === 2){
@@ -7,4 +7,9 @@ function getComputerChoice(){
     } else {
         return "paper";
     }
+}
+
+function getHumanChoice(){
+    userChoice = prompt("Please enter Rock, Paper or Scissors:", "");
+    return userChoice;
 }
