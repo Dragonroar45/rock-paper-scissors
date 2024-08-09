57651d75664de58810dfcc153a7077bc4102a40f
Dragonroar45 <malhotraharsh098@gmail.com>
2024-08-09 14:11:51 +0530

    Partially complete playround and make playgame functions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getComputerChoice(){
     if (numComputerChoice === 1){
         return "rock";
     } else if (numComputerChoice === 2){
-        return "scissor";
+        return "scissors";
     } else {
         return "paper";
     }
@@ -11,9 +11,40 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     userChoice = prompt("Please enter Rock, Paper or Scissors:", "");
-    return userChoice;
+    return userChoice.toLowerCase();
 }
 
+function playRound(humanChoice, computerChoice){
+    let round = 0;
+    while (round <= 5){
+        round += 1;
+        if (humanChoice === computerChoice){
+            console.log("It's a tie");
+            humanScore += 1;
+            computerScore += 1;
+            console.log(`Current scores: Computer - ${computerScore} Human - ${humanScore}`);
+            humanChoice = getHumanChoice();
+            computerChoice = getComputerChoice();
+            continue;
+        } else if (humanChoice === "rock" && computerChoice === "paper"){
+            console.log("You lose. Paper beats Rock");
+            computerScore += 1;
+            console.log(`Current scores: Computer - ${computerScore} Human - ${humanScore}`);
+            humanChoice = getHumanChoice();
+            computerChoice = getComputerChoice();
+            continue;
+        } else if (humanChoice === "rock" && computerChoice === "scissors"){
+            console.log("You win. Rock beats Scissors");
+            humanScore += 1;
+            console.log(`Current scores: Computer - ${computerScore} Human - ${humanScore}`);
+            humanChoice = getHumanChoice();
+            computerChoice = getComputerChoice();
+            continue;
+        } else if (humanChoice === "scissors" && computerChoice === "rock"){
+            console.log("You lose")
+        }
+    }
+}
 let computerChoice = getComputerChoice();
 let humanChoice = getHumanChoice();
 let humanScore = 0;
